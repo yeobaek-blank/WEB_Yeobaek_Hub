a9a28697ebd0bd3300fdcb3a7599471251802ef2
gyuriiiiii <157960770+gyuriiiiii@users.noreply.github.com>
2025-07-25 13:52:55 +0900

    Update tailwind.config.js
    250725 2차 수정

--- yeobaek-hub/tailwind.config.js
+++ yeobaek-hub/tailwind.config.js
@@ -1,10 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nightsky: '#3E3B4F',
+        classicblue: '#34558B',
+        provence: '#A3D3FF',
+        babyblue: '#D1E8FF',
+        monument: '#A9B1B7',
+      },
+    },
   },
   plugins: [],
 }
